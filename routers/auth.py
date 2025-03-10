f430e60abf8813c5848c7abb69b5cada6c057e8d
Vladyslav Tyshchenko <vlad.tiw27@gmail.com>
2025-03-10 21:34:38 +0100

    update: add create user logic

--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,24 @@
-from fastapi import APIRouter
+from typing import Annotated
+from sqlalchemy.orm import Session
+from starlette import status
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
+from database import SessionLocal
+
 
 router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -16,17 +31,17 @@ class CreateUserRequest(BaseModel):
     last_name: str
     role: str
 
-@router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest):
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
 
-
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
