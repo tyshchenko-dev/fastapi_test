cc141375f30d3e9598fea45d26ad78143e1b1809
Vladyslav Tyshchenko <vlad.tiw27@gmail.com>
2025-03-10 21:02:44 +0100

    first commit

---
+++ routers/auth.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
+
+router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    password: str
+    first_name: str
+    last_name: str
+    role: str
+
+@router.post("/auth")
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=create_user_request.password,
+        is_active=True
+    )
+
+
+    return create_user_model
